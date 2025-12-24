794a0b37a89e2cdf4fcde374f7f31de402ecd824
hungnn2810 <hungnn2810@gmail.com>
2025-12-24 09:52:30 +0700

    test ci

--- src/LifeBalance.Application/Auth/Commands/Handlers/RegisterUserHandler.cs
+++ src/LifeBalance.Application/Auth/Commands/Handlers/RegisterUserHandler.cs
@@ -1,12 +1,13 @@
+using LifeBalance.Application.Services.Abstractions;
 using LifeBalance.Application.SharedKernel.Models;
 using MediatR;
 
 namespace LifeBalance.Application.Auth.Commands.Handlers;
 
-public class RegisterUserHandler : IRequestHandler<RegisterUser, BaseResponse>
+public class RegisterUserHandler(IAuthService service) : IRequestHandler<RegisterUser, BaseResponse>
 {
     public async Task<BaseResponse> Handle(RegisterUser request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await service.RegisterAsync(request);
     }
 }
